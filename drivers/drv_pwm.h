03e92baf16c949ffafaef28768affb3fbe473d36
Wenlong.Fan <fanwenl_g@126.com>
2018-11-21 18:57:06 +0800

    [Drivers]add PWM driver
    Signed-off-by: Wenlong.Fan <fanwenl_g@126.com>

---
+++ drivers/drv_pwm.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-22      fanwenl      1st version
+ */
+
+#ifndef __DRV_PWM_H__
+#define __DRV_PWM_H__
+
+#include "wm_pwm.h"
+#include "wm_gpio_afsel.h"
+
+int wm_pwm_init(void);
+
+#endif
