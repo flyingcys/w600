5febc4cfc80688d64528dc60d9cc7e7f822950fe
Wenlong.Fan <fanwenl_g@126.com>
2018-11-22 15:26:44 +0800

    [Drivers]add adc drivers

---
+++ drivers/drv_adc.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2006-2018, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2018-11-22      fanwenl      1st version
+ */
+#ifndef __DRV_ADC_H
+#define __DRV_ADC_H
+
+#include "wm_io.h"
+#include "wm_adc.h"
+#include "wm_gpio_afsel.h"
+
+int wm_adc_init(void);
+
+#endif // !__DRV_ADC_H
+
